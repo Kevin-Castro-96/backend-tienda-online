3f2dc33cbab379696f8c6630aa3a94c05bf77382
Kevin Castro <street123castle@gmail.com>
2025-08-19 21:06:52 -0300

    preparo datasource para produccion

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,22 +1,21 @@
 import { DataSource } from "typeorm";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs";
 import { User } from "../entities/User";
 import { Credential } from "../entities/Credential";
 import { Order } from "../entities/Order";
 import { Category } from "../entities/Category";
 import { Product } from "../entities/Product";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  synchronize: true,
-  dropSchema: true,
+  url: process.env.DATABASE_URL, // Render te da esta URL completa
+  synchronize: true, // ⚠️ solo para desarrollo
   logging: false,
   entities: [User, Credential, Order, Product, Category],
-  subscribers: [],
-  migrations: [],
+  ssl: {
+    rejectUnauthorized: false, // necesario en Render
+  },
 });
+
